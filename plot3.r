e72e4fdde4923e5342dccab786bf582db45f811a
Walter Burrough <walterlsb@users.noreply.github.com>
2014-07-13 15:54:33 -0500

    Updated plot3.r to match legend in plot4

--- plot3.r
+++ plot3.r
@@ -32,7 +32,7 @@ plot3<-function(dataIn){
         with(dataIn, points(DT, Sub_metering_1, col="black",type="l"))
         with(dataIn, points(DT, Sub_metering_2, col="red",type="l"))
         with(dataIn, points(DT, Sub_metering_3, col="blue",type="l"))
-        legend("topright",pch=1,col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+        legend("topright",lty=1,col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
         dev.off()
 }
 
